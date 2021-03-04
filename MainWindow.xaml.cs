d214c0d90bc183ad3a31aba3322e2aae31df2297
Mark Curran <S00199895@mail.itsligo.ie>
2021-03-04 10:47:31 +0000

    set item source for the main 3 list boxes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace lab6
 {
@@ -20,9 +9,36 @@ namespace lab6
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		NORTHWNDEntities db = new NORTHWNDEntities();
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			//Populate stock level lbx
+			lbx_stock.ItemsSource = Enum.GetNames(typeof(StockLevel));
+
+			var query1 = from s in db.Suppliers
+						 orderby s.CompanyName
+						 select new
+						 {
+							 SupplierName = s.CompanyName,
+							 SupplierID = s.SupplierID,
+							 Country = s.Country
+						 };
+
+			lbx_suppliers.ItemsSource = query1.ToList();
+
+			var query2 = query1
+				.OrderBy(s => s.Country)
+				.Select(s => s.Country);
+
+			var countries = query2.ToList();
+			lbx_countries.ItemsSource = countries.Distinct();
+
+			public enum StockLevel { Low, Normal, Overstocked };
 		}
 	}
 }
